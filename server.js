8c5a391f33386b43f47b126a589c5b81f7dbc888
superlgtele <superlgtele@naver.com>
2022-01-03 18:13:54 +0900

    RestAPI
    2021/01/03

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-app.listen(5000, function(){
-    console.log("Start 5000 Server!");
+const db = mongoose.connection;
+mongoose.connect("mongodb://localhost:27017/test");
+db.once("open", function () {
+  console.log("DB connected");
+});
+db.on("error", function (err) {
+  console.log("DB ERROR : ", err);
 });
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(5000, function () {
+  console.log("Start 5000 Server!");
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/write", function (req, res) {
+  res.sendFile(__dirname + "/write.html");
+});
+
+app.get("/list", function (req, res) {
+  db.collection("practice")
+    .find()
+    .toArray(function (err, result) {
+      res.render("list.ejs", { posts: result });
+    });
+});
+
+app.post("/add", function (req, res) {
+  res.send("전송완료");
+  db.collection("practice").insertOne(
+    { title: req.body.title, date: req.body.date },
+    function (err, result) {
+      console.log("저장완료");
+    }
+  );
 });
