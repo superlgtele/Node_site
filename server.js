2af59806883ffbd057dc9a8169976902a7c66169
superlgtele <superlgtele@naver.com>
2022-01-04 23:06:02 +0900

    ajax
    2022/01/04

--- server.js
+++ server.js
@@ -36,12 +36,32 @@ app.get("/list", function (req, res) {
     });
 });
 
+app.delete("/delete", function (req, res) {});
+
 app.post("/add", function (req, res) {
   res.send("전송완료");
-  db.collection("practice").insertOne(
-    { title: req.body.title, date: req.body.date },
+  // 콜렉션: counter, name: boardnumber인 데이터 = result
+  db.collection("counter").findOne(
+    { name: "boardnumber" },
     function (err, result) {
-      console.log("저장완료");
+      // practice 콜렉션에 id: result의 totalPost값, title/date는 사용자 입력값을 넣음
+      const allboardnum = result.totalPost;
+      db.collection("practice").insertOne(
+        { _id: allboardnum + 1, title: req.body.title, date: req.body.date },
+        function (err, result) {
+          console.log("저장완료");
+          // 위의 코드가 성공하면 collection: counter 에서 name:boardnumber 인 데이터의 totalPost 값에 1을 더함($increase)
+          db.collection("counter").updateOne(
+            { name: "boardnumber" },
+            { $inc: { totalPost: 1 } },
+            function (err, result) {
+              if (err) {
+                return console.log(err);
+              }
+            }
+          );
+        }
+      );
     }
   );
 });
